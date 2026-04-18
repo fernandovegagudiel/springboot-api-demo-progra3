0c5d8d986a1367d6b42835273365aa1c7a41b3d3
Fernando <fvegag1@miumg.edu.gt>
2026-04-17 21:29:52 -0600

    Terminacion completa de la recomendacion

--- src/main/java/com/ejemplo/demo/api/controller/PrestamoController.java
+++ src/main/java/com/ejemplo/demo/api/controller/PrestamoController.java
@@ -2,16 +2,17 @@ package com.ejemplo.demo.api.controller;
 
 import com.ejemplo.demo.api.dto.PrestamoRequest;
 import com.ejemplo.demo.api.dto.PrestamoResponse;
-import com.ejemplo.demo.domain.service.PrestamoService;
+import com.ejemplo.demo.domain.service.PrestamoService; 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import com.ejemplo.demo.domain.model.Prestamo;
 import org.springframework.web.bind.annotation.*;
+import com.ejemplo.demo.domain.model.Prestamo;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/simulaciones")
-@Tag(name = "Simulador de Préstamos", description = "Reto Paso 8")
+@Tag(name = "Simulador de Préstamos")
 public class PrestamoController {
 
     private final PrestamoService prestamoService;
@@ -21,8 +22,20 @@ public class PrestamoController {
     }
 
     @PostMapping("/prestamo")
-    @Operation(summary = "Calcula la cuota de un préstamo")
+    @Operation(summary = "Calcula la simulación")
     public PrestamoResponse simular(@Valid @RequestBody PrestamoRequest request) {
         return prestamoService.calcular(request);
+    }
+    
+    @GetMapping
+    @Operation(summary = "Obtiene la lista de todos los préstamos guardados")
+    public List<Prestamo> listarTodos() {
+        return prestamoService.obtenerTodos();
+    }
+    
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina un préstamo por su ID")
+    public void eliminar(@PathVariable Long id) {
+        prestamoService.eliminarPrestamo(id);
     }
 }
